c689958e303f51a4e50c06c772ba716b60b303db
Mitch-Tyrer <mltcinematography@gmail.com>
2019-02-23 13:15:05 -0700

    updated the templates to use the included data

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
-const { data } = require('./data/data.json');
+const data = require('./data/data.json');
+const projects = data.projects
+
 
 const app = express();
 
@@ -12,15 +14,16 @@ app.use('/static', express.static('public'));
 app.set('view engine', 'pug');
 
 app.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {projects });
 }); // end homepage get
 
 app.get('/about', (req, res) => {
     res.render('about');
 }); // end about page get
 
-app.get('/projects', (req, res) => {
-    res.render('project');
+app.get('/projects/:id', (req, res) => {
+    const id = req.params.id
+    res.render('project', {id, projects});
 }); // end projects get route
 
 
