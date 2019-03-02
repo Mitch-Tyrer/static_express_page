76b79a675303f6e43c09459273c4016e72861821
Mitch-Tyrer <mltcinematography@gmail.com>
2019-03-02 13:15:50 -0700

    added final touches

--- app.js
+++ app.js
@@ -24,6 +24,7 @@ app.use((req, res, next) => {
 
 app.use((err, req, res, next) => {
     res.locals.error = err;
+    console.error(err.status, err.message)
     res.status(err.status);
     res.render('error');
   });
